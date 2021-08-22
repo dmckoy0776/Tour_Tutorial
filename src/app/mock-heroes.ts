231390695d4532b78d6a0e4eb51e3d1b95be46b8
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-08-21 21:14:45 -0400

    fixed the list component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Ray Gun' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magnetomic' },
+  { id: 16, name: 'RubberBandMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Hawkin IQ' },
+  { id: 19, name: 'Magmatician' },
+  { id: 20, name: 'Tornado' }
+];
