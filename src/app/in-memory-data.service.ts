d6a6230532295c0c38e08ac4892d97739bd133c2
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-09-18 17:11:38 -0400

    finally fixed detail componenent

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -26,5 +26,5 @@ export class InMemoryDataService implements InMemoryDbService{
   genId(heroes: Hero[]): number {
     return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
-  constructor() { }
+  
 }
